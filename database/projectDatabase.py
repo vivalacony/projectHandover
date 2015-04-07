69998ed1bb6bd9ac0a7d776a2151ec703973d930
vivalacony <cofallon@tcd.ie>
2015-04-07 14:26:41 +0100

    Update projectDatabase.py

--- database/projectDatabase.py
+++ database/projectDatabase.py
@@ -1,4 +1,5 @@
 import redis
+import MySQLdb
 
 keyFormat = 'project_{0}'
 projectRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )#TODO: move to config file
@@ -23,11 +24,25 @@ def addNewProject(secretUrl, Uname, projectId, title, Author, SourceCode, Descri
 	projectRedisDB.hmset(key+'_info', projectInfo)
 	for tag in tags:
 		projectRedisDB.sadd(key+'_tags', tag)
+		
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE UserName LIKE '" + Uname + "'")
+	#AddedByID=cur.fetchone()
+  
+	#cur.execute("INSERT INTO CNGL_Webservices_Tbl (Name,Description,SourceCodeLink,Author,AddedByID,ExpectedInput,Output) VALUES ('" + title + "', '" + Description + "','" + SourceCode + "', '" + "','" + Author + "'," + AddedByID +",'" + Input +"','" + Output + "'")
 
 def removeProject(projectId):
 	key = _projectKey(projectId)
 	projectRedisDB.delete(key+'_info')
 	projectRedisDB.delete(key+'_tags')
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	
+	#cur.execute("DELETE FROM CNGL_Webservices_Tbl WHERE WebserviceID = " + projectId)
 
 def getProjectInfo(projectId):
 	key = _projectKey(projectId)
