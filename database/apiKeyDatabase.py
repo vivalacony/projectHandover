3b7ab74c9cc6a4365c2c83739d303e83c3fbafb8
vivalacony <cofallon@tcd.ie>
2015-04-07 14:19:05 +0100

    Update apiKeyDatabase.py

--- database/apiKeyDatabase.py
+++ database/apiKeyDatabase.py
@@ -1,4 +1,5 @@
 import redis
+import MySQLdb
 
 keyFormat = 'apiKey_{0}'
 apiKeyRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )
@@ -6,14 +7,36 @@ apiKeyRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )
 def addApiKey( apiKey, userId ):
   key = _apiKeyKey(apiKey)
   apiKeyRedisDB.set(key, userId)
+  #The following can only be implemented after the MySQL database has been initialised on the server
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+  #cur = db.cursor()
+  
+  #cur.execute("UPDATE CNGL_User_Tbl SET APIKey = '" + apiKey + "' WHERE UserID = " + userId)
 
 def removeApiKey( apiKey ):
   key = _apiKeyKey(apiKey)
   apiKeyRedisDB.delete(key)
+  #The following can only be implemented after the MySQL database has been initialised on the server
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+  #cur = db.cursor()
+  
+  #cur.execute("UPDATE CNGL_User_Tbl SET APIKey = NULL WHERE APIKey LIKE '" + apiKey + "'")
+  
 
 def getApiKeyUserId(apiKey):
   key = _apiKeyKey(apiKey)
   return apiKeyRedisDB.get(key)
+  
+  #The following can only be implemented after the MySQL database has been initialised on the server
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+  #cur = db.cursor()
+  
+  #cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE APIKey LIKE '" + apiKey + "'")
+  
+  #return cur.fetchone()
 
 def _apiKeyKey(apiKey):
   return keyFormat.format(apiKey)
