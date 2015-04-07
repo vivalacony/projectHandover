7cfd973c95521dc0ca6a544e490aa5d504801d23
vivalacony <cofallon@tcd.ie>
2015-04-07 14:24:59 +0100

    Update pendingUserDatabase.py

--- database/pendingUserDatabase.py
+++ database/pendingUserDatabase.py
@@ -1,4 +1,5 @@
 import redis
+import MySQLdb
 
 PENDING_USER_LIST_KEY = 'list_of_pending_users'
 
@@ -16,11 +17,21 @@ def addUser(userId, email, username, passwordHash):
 
   key = _pendingKey(userId)
   pendingRedisDB.hmset(key, user)
+  #The following can only be implemented after the MySQL database has been initialised on the server
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("INSERT INTO CNGL_User_Tbl (EmailAddress, UserName,Password) VALUES('" + email + "','"+username+"','"+passwordHash+"')")
 
 def removeUser(userId):
   _removeFromPendingUserList(userId)
   key = _pendingKey(userId)
   pendingRedisDB.delete(key)
+  #The following can only be implemented after the MySQL database has been initialised on the server
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("DELETE FROM CNGL_User_Tbl WHERE UserID = " + userId)
 
 def getUserInfo(userId):
   key = _pendingKey(userId)
