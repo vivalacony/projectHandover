d29c43d3672a9551c96a1889692ffd5d644d1b5e
vivalacony <cofallon@tcd.ie>
2015-04-07 14:27:14 +0100

    Update statsDatabase.py

--- database/statsDatabase.py
+++ database/statsDatabase.py
@@ -1,4 +1,5 @@
 import redis
+import MySQLdb
 
 keyUserFormat = 'stats_user_{0}_{1}'
 keyProjectFormat = 'stats_project_{0}'
@@ -7,6 +8,12 @@ statsRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )
 def addUserHit(statId, userId, projectId, time):
 	key = _statsUserKey(userId, projectId)
 	statsRedisDB.zadd(key, time, statId)
+	
+	#db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	
+	#cur.execute("INSERT INTO CNGL_UserAnalytics_Tbl (UserID, WebServiceID) VALUES ('" + userId + "','"+ projectId +"')" )
 
 def getAllUserHits( userId, projectId ):
 	key = _statsUserKey(userId, projectId)
@@ -27,4 +34,3 @@ def _statsUserKey(userId, projectId):
 
 def _statsProjectKey(projectId):
   return keyProjectFormat.format(projectId)
-
