6a5f0fd59fffd4bcbae604a8d39ad6697f5c2b81
vivalacony <cofallon@tcd.ie>
2015-04-07 14:24:31 +0100

    Update emailDatabase.py

--- database/emailDatabase.py
+++ database/emailDatabase.py
@@ -1,4 +1,5 @@
 import redis
+import MySQLdb
 
 keyFormat = 'email_{0}'
 emailRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )
@@ -6,14 +7,29 @@ emailRedisDB = redis.StrictRedis( '127.0.0.1', 6379 )
 def addEmail( email, userId ):
   key = _emailKey(email)
   emailRedisDB.set(key, userId)
+  
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET EmailAddress = '" + email + "' WHERE UserID = " + userId)
 
 def removeEmail( email ):
   key = _emailKey(email)
   emailRedisDB.delete(key)
+  
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("UPDATE CNGL_User_Tbl SET EmailAddress = '' WHERE UserID = " + userId)
 
 def getEmailUserId(email):
   key = _emailKey(email)
   return emailRedisDB.get(key)
+  
+  #db=MySQLdb.connect(host="localhost",user="root",passwd="woot",db="TestDB")
+  
+	#cur = db.cursor()
+	#cur.execute("SELECT UserID FROM CNGL_User_Tbl WHERE EmailAddress LIKE '" + email + "'")
 
 def _emailKey(email):
   return keyFormat.format(email)
